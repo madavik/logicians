bef60edf0a5d864b4831d55dda6a2358bc508651
joey gibson <joe.vgibson@gmail.com>
2015-06-03 19:02:22 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guild::Application.config.secret_token = 'e2492664be747c8b98c4cae586b5a5c4e9d44d754a621e9476fa438439297c240cdccb16ae6b6015fa3cb363cd4b7493f301bc8d4fb40501620942cf514df458'
